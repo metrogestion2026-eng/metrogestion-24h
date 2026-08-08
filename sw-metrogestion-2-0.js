530dab71bdb8f2cf2852876a140c65dd26c3ce4a
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-08 20:31:37 +0200

    Publicar versión 30

--- sw-metrogestion-2-0.js
+++ sw-metrogestion-2-0.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gestion-metrogestion-2-0-v29';
+const CACHE_NAME = 'gestion-metrogestion-2-0-v30';
 const APP_FILES = [
   './metrogestion-2-0.html',
   './manifest-metrogestion-2-0.json',
