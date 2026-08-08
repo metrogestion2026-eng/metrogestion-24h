90c8e8a4da94c3802d546ab0f35325a74a1be26e
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-08 11:18:41 +0200

    Renovar caché del mensaje de guardado

--- sw-metrogestion-2-0.js
+++ sw-metrogestion-2-0.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gestion-metrogestion-2-0-v5';
+const CACHE_NAME = 'gestion-metrogestion-2-0-v6';
 const APP_FILES = [
   './metrogestion-2-0.html',
   './manifest-metrogestion-2-0.json',
