19bdd33046fbbda44d0687d9c4965f0bd5761c0d
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-08 13:59:28 +0200

    Actualiza caché del calendario de entrada

--- sw-metrogestion-2-0.js
+++ sw-metrogestion-2-0.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gestion-metrogestion-2-0-v19';
+const CACHE_NAME = 'gestion-metrogestion-2-0-v20';
 const APP_FILES = [
   './metrogestion-2-0.html',
   './manifest-metrogestion-2-0.json',
