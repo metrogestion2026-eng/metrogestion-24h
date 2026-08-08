bbb3eaa6e7a55748b2aa1c40780927437b446cad
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-08 15:20:44 +0200

    Actualiza caché del desplegable de etapas

--- sw-metrogestion-2-0.js
+++ sw-metrogestion-2-0.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gestion-metrogestion-2-0-v22';
+const CACHE_NAME = 'gestion-metrogestion-2-0-v23';
 const APP_FILES = [
   './metrogestion-2-0.html',
   './manifest-metrogestion-2-0.json',
