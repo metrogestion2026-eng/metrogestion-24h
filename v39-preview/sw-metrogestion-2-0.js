f4e79a7185c091ed05222c299d6352f6ee9c9ddf
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-13 10:23:16 +0200

    v39: cargar plegables de T programadas y Reservas

--- v39-preview/sw-metrogestion-2-0.js
+++ v39-preview/sw-metrogestion-2-0.js
@@ -15,6 +15,7 @@ self.fetch = async (input, init) => {
       if (!html.includes('hotel-v39-fix-reservas.js')) html = html.replace('</body>','<script src="./hotel-v39-fix-reservas.js?v=39-20260813c"></script></body>');
       if (!html.includes('hotel-v39-editar-parada.js')) html = html.replace('</body>','<script src="./hotel-v39-editar-parada.js?v=39-20260813d"></script></body>');
       if (!html.includes('hotel-v39-t-programadas.js')) html = html.replace('</body>','<script src="./hotel-v39-t-programadas.js?v=39-20260813e"></script></body>');
+      if (!html.includes('hotel-v39-plegables.js')) html = html.replace('</body>','<script src="./hotel-v39-plegables.js?v=39-20260813f"></script></body>');
       const headers = new Headers(response.headers);
       headers.set('Content-Type','text/html; charset=utf-8');
       headers.set('Cache-Control','no-store');
