a629edbcd9dc966b88210db01c4ea32aa5cb9cd4
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-13 10:16:17 +0200

    v39 preview: activar T programadas completas

--- v39-preview/sw-metrogestion-2-0.js
+++ v39-preview/sw-metrogestion-2-0.js
@@ -14,6 +14,7 @@ self.fetch = async (input, init) => {
       if (!html.includes('hotel-v39-integracion.js')) html = html.replace('</body>','<script src="./hotel-v39-integracion.js?v=39-20260813"></script></body>');
       if (!html.includes('hotel-v39-fix-reservas.js')) html = html.replace('</body>','<script src="./hotel-v39-fix-reservas.js?v=39-20260813c"></script></body>');
       if (!html.includes('hotel-v39-editar-parada.js')) html = html.replace('</body>','<script src="./hotel-v39-editar-parada.js?v=39-20260813d"></script></body>');
+      if (!html.includes('hotel-v39-t-programadas.js')) html = html.replace('</body>','<script src="./hotel-v39-t-programadas.js?v=39-20260813e"></script></body>');
       const headers = new Headers(response.headers);
       headers.set('Content-Type','text/html; charset=utf-8');
       headers.set('Cache-Control','no-store');
