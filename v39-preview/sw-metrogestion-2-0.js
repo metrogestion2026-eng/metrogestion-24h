e5543b30532a8de3f91605c2d4fbeb715261fc87
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-13 12:50:51 +0200

    v39: cargar ficha de taller con contactos múltiples

--- v39-preview/sw-metrogestion-2-0.js
+++ v39-preview/sw-metrogestion-2-0.js
@@ -20,6 +20,7 @@ self.fetch = async (input, init) => {
       if (!html.includes('hotel-v39-ajustes-operativos.js')) html = html.replace('</body>','<script src="./hotel-v39-ajustes-operativos.js?v=39-20260813j"></script></body>');
       if (!html.includes('hotel-v39-reordenar-t.js')) html = html.replace('</body>','<script src="./hotel-v39-reordenar-t.js?v=39-20260813k"></script></body>');
       if (!html.includes('hotel-v39-permisos-talleres.js')) html = html.replace('</body>','<script src="./hotel-v39-permisos-talleres.js?v=39-20260813m"></script></body>');
+      if (!html.includes('hotel-v39-taller-contactos-multiples.js')) html = html.replace('</body>','<script src="./hotel-v39-taller-contactos-multiples.js?v=39-20260813n"></script></body>');
       const headers = new Headers(response.headers);
       headers.set('Content-Type','text/html; charset=utf-8');
       headers.set('Cache-Control','no-store');
