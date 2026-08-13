7c21f4701e6ce340873028cb8503228e2f13d898
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-13 09:30:33 +0200

    v39 preview: cargar correccion de reservas liberadas

--- v39-preview/sw-metrogestion-2-0.js
+++ v39-preview/sw-metrogestion-2-0.js
@@ -12,6 +12,7 @@ self.fetch = async (input, init) => {
       html = html.replace('Activar 24H · Beta 2.0 · v36','Metrogestión · v39 · PRUEBAS');
       html = html.replace('Gestión de mantenimientos · Activar 24H','Gestión de Mantenimiento · Metrogestión v39');
       if (!html.includes('hotel-v39-integracion.js')) html = html.replace('</body>','<script src="./hotel-v39-integracion.js?v=39-20260813"></script></body>');
+      if (!html.includes('hotel-v39-fix-reservas.js')) html = html.replace('</body>','<script src="./hotel-v39-fix-reservas.js?v=39-20260813c"></script></body>');
       const headers = new Headers(response.headers);
       headers.set('Content-Type','text/html; charset=utf-8');
       headers.set('Cache-Control','no-store');
