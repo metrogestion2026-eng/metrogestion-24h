4b5ba618648aec6829c9c38d4a0a7a617c89c4e8
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-14 10:39:55 +0200

    v39: cargar fecha real de pizarra sin autoabrir Hotel

--- v39-preview/sw-metrogestion-2-0.js
+++ v39-preview/sw-metrogestion-2-0.js
@@ -39,6 +39,7 @@ self.fetch = async (input, init) => {
       if (!html.includes('hotel-v39-sin-ver-expediente.js')) html = html.replace('</body>','<script src="./hotel-v39-sin-ver-expediente.js?v=39-20260813t"></script></body>');
       if (!html.includes('hotel-v39-nota-admin.js')) html = html.replace('</body>','<script src="./hotel-v39-nota-admin.js?v=39-20260813u"></script></body>');
       if (!html.includes('hotel-v39-reservas-solo-lectura.js')) html = html.replace('</body>','<script src="./hotel-v39-reservas-solo-lectura.js?v=39-20260814h"></script></body>');
+      if (!html.includes('hotel-v39-pizarra-actual-fix.js')) html = html.replace('</body>','<script src="./hotel-v39-pizarra-actual-fix.js?v=39-20260814i"></script></body>');
       const headers = new Headers(response.headers);
       headers.set('Content-Type','text/html; charset=utf-8');
       headers.set('Cache-Control','no-store');
