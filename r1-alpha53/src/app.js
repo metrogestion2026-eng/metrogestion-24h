7fb5959bc5289a423a79160c89b8d45c930b45c2
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-25 16:10:18 +0200

    Alpha53: añadir aplicación base

---
+++ r1-alpha53/src/app.js
@@ -0,0 +1,5 @@
+import '../../r1-alpha52/src/app.js';
+
+const VERSION = 'r1.0.0-alpha.53';
+const versionNode = document.querySelector('#app-version');
+if (versionNode) versionNode.textContent = VERSION;
