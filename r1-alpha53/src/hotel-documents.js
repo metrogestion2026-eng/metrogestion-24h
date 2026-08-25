090bff60ca12d7830fbcf7013a5e5f739d055490
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-25 16:15:22 +0200

    Alpha53: exponer módulo documental

---
+++ r1-alpha53/src/hotel-documents.js
@@ -0,0 +1,5 @@
+export {
+  HOTEL_DOCUMENT_BUCKET,MAX_DOCUMENT_BYTES,openStoredDocument,downloadStoredDocument,
+  loadDocumentsForGroups,loadAllDocuments,
+} from './document-core.js';
+export { createStageDocuments,summarizeDocuments } from './stage-documents.js';
