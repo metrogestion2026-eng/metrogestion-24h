bd9fd95bab825bee78aa7a297382f6abcdf2cd3d
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-26 05:23:19 +0200

    Alpha54: añadir aplicación principal

---
+++ r1-alpha54/src/app.js
@@ -0,0 +1,5 @@
+import '../../r1-alpha53/src/app.js';
+
+const VERSION = 'r1.0.0-alpha.54';
+const versionNode = document.querySelector('#app-version');
+if (versionNode) versionNode.textContent = VERSION;
