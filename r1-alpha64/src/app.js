8c2cccfe17f8bac408147c1bf2aa698ac5725c7c
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-27 14:00:10 +0200

    Alpha64: iniciar gestión nativa de recuperación de dispositivos

---
+++ r1-alpha64/src/app.js
@@ -0,0 +1,6 @@
+import '../../r1-alpha62/src/app.js';
+import './device-admin.js';
+
+const VERSION = 'r1.0.0-alpha.64';
+const versionNode = document.querySelector('#app-version');
+if (versionNode) versionNode.textContent = VERSION;
