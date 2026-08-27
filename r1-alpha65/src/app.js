027cc0c3f79e0b81e433b7b226000140e61704c8
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-27 15:45:03 +0200

    Alpha65: iniciar seguridad de cuenta y sugerencias

---
+++ r1-alpha65/src/app.js
@@ -0,0 +1,7 @@
+import '../../r1-alpha64/src/app.js';
+import './account-security.js';
+import './suggestions.js';
+
+const VERSION = 'r1.0.0-alpha.65';
+const versionNode = document.querySelector('#app-version');
+if (versionNode) versionNode.textContent = VERSION;
