b8b023868dbf86e3ed8432ef23849c252ab7d21d
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-27 09:08:18 +0200

    Alpha62: iniciar navegación coordinada

---
+++ r1-alpha62/src/app.js
@@ -0,0 +1,6 @@
+import './navigation-controller.js';
+import '../../r1-alpha61/src/app.js';
+
+const VERSION = 'r1.0.0-alpha.62';
+const versionNode = document.querySelector('#app-version');
+if (versionNode) versionNode.textContent = VERSION;
