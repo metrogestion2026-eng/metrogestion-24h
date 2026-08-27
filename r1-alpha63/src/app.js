89db9bd17fccaa548d1cbd9969cb632175301746
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-27 09:28:33 +0200

    Alpha63: iniciar gestión de dos dispositivos

---
+++ r1-alpha63/src/app.js
@@ -0,0 +1,6 @@
+import '../../r1-alpha62/src/app.js';
+import './device-admin.js';
+
+const VERSION = 'r1.0.0-alpha.63';
+const versionNode = document.querySelector('#app-version');
+if (versionNode) versionNode.textContent = VERSION;
