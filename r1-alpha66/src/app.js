77e7cdbbd2abb6f4dc7d28cc81bfcb7ccca924ad
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-28 14:13:39 +0200

    Alpha66: fechas operativas y días por periodo en fichas

---
+++ r1-alpha66/src/app.js
@@ -0,0 +1,5 @@
+import '../../r1-alpha65/src/app.js';
+
+const VERSION = 'r1.0.0-alpha.66';
+const versionNode = document.querySelector('#app-version');
+if (versionNode) versionNode.textContent = VERSION;
