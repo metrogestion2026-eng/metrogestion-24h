95d00aa22ff19c3f7bb284fff51993c26492d7aa
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-29 13:38:26 +0200

    Alpha68: iniciar control diario de flota

---
+++ r1-alpha68/src/app.js
@@ -0,0 +1,6 @@
+import '../../r1-alpha67/src/app.js';
+import './manteniment-sync.js';
+
+const VERSION = 'r1.0.0-alpha.68';
+const versionNode = document.querySelector('#app-version');
+if (versionNode) versionNode.textContent = VERSION;
