1446b1a1f52dce8988790046fd50926b926cc5e1
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-30 18:02:15 +0200

    Alpha69: cargar control de presencia

---
+++ r1-alpha69/src/app.js
@@ -0,0 +1,6 @@
+import '../../r1-alpha68/src/app.js';
+import './presence-security.js';
+
+const VERSION = 'r1.0.0-alpha.69';
+const versionNode = document.querySelector('#app-version');
+if (versionNode) versionNode.textContent = VERSION;
