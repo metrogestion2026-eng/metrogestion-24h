4e89a077fc68eab5a90a7d38613514c155997478
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-08-31 12:07:46 +0200

    Alpha70: mostrar teléfono 24H según fabricante

---
+++ r1-alpha70/src/app.js
@@ -0,0 +1,6 @@
+import '../../r1-alpha69/src/app.js';
+import './assistance-protocols.js';
+
+const VERSION = 'r1.0.0-alpha.70';
+const versionNode = document.querySelector('#app-version');
+if (versionNode) versionNode.textContent = VERSION;
