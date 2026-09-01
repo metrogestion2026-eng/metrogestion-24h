265cf419c1f8ef0bf738fe2e7277e0f64b58d1ba
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-09-01 05:47:03 +0200

    Alpha70: activar buscador reparado del Historico

--- r1-alpha70/src/app.js
+++ r1-alpha70/src/app.js
@@ -1,5 +1,6 @@
 import '../../r1-alpha69/src/app.js';
 import './assistance-protocols.js';
+import './history-search.js';
 
 const VERSION = 'r1.0.0-alpha.70';
 const versionNode = document.querySelector('#app-version');
