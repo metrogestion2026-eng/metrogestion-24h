32373e410fe9e4925cd7f8d201262d87090202f2
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-09-02 06:07:31 +0200

    fix(alpha70): corregir la última T en Histórico

--- r1-alpha70/src/history-card.js
+++ r1-alpha70/src/history-card.js
@@ -2,7 +2,7 @@ import { detail, element } from '../../r1-alpha17/src/dom.js';
 import { openHotelEditor } from './hotel-editor.js';
 import { createStageDocuments, summarizeDocuments } from '../../r1-alpha67/src/hotel-documents.js';
 import { openStageDetail } from '../../r1-alpha67/src/stage-detail.js';
-import { createOperationalDates, createSubstitutionBilling } from '../../r1-alpha67/src/card-operational.js';
+import { createOperationalDates, createSubstitutionBilling } from './card-operational.js';
 import { createQuickStageControl } from '../../r1-alpha67/src/stage-quick.js';
 
 export function formatDateTime(value) {
