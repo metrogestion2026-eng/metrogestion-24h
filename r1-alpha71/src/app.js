9a94f9e48d794e3c1b2a93479ee7c0352ab2f658
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-09-03 11:16:28 +0200

    fix(alpha71): buscar fichas en todo el historico

--- r1-alpha71/src/app.js
+++ r1-alpha71/src/app.js
@@ -3,7 +3,6 @@ import './assistance-protocols.js';
 import './assistance-call-continuity.js';
 import './listados-export.js';
 import './reservas-create.js';
-import './history-search.js';
 import './activos.js';
 
 const VERSION = 'r1.0.0-alpha.71';
