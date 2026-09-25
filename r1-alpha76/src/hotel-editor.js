e208f7ba5f0ac4fe18681ed11a6c29810cf79728
metrogestion2026-eng <metrogestion2026@gmail.com>
2026-09-25 09:23:24 +0200

    Actualizar editor de trabajos 76.9

--- r1-alpha76/src/hotel-editor.js
+++ r1-alpha76/src/hotel-editor.js
@@ -2,7 +2,7 @@ import { element } from '../../r1-alpha17/src/dom.js';
 import { revealStagesFor } from '../../r1-alpha75/src/stages-collapse.js?v=75.18';
 import { supabase } from '../../r1-alpha17/src/supabase.js';
 import { renderMainSections } from './hotel-editor-main.js?v=75.1';
-import { renderStagesSection, stagesPayloadWithCatalogues } from './hotel-editor-stages.js?v=76.5';
+import { renderStagesSection, stagesPayloadWithCatalogues } from './hotel-editor-stages.js?v=76.9';
 import { manualAnnotationsPayload, renderManualAnnotationsEditor } from './annotations.js';
 import { saveErrorIssues, stageStateMismatchIssues } from './hotel-editor-validation.js';
 import {
